f1a31c7c128759812dc58754bbfbc4338c78c0a3
wamweru <49023099+wamweru@users.noreply.github.com>
2019-05-23 12:14:55 +0300

    Update 1northcat.php

--- admin/1northcat.php
+++ admin/1northcat.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Me.report | Admin</title>
-    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="description" >
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
